8c0dacf364c49a0a836419c2c5f34305d22b7673
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2020-04-27 11:59:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1) ;
+var randomNumber2 = Math.floor((Math.random()*6)+1) ;
+
+document.querySelector(".img1").setAttribute("src" , "images\\dice" + randomNumber1 +".png");
+document.querySelector(".img2").setAttribute("src" , "images\\dice" + randomNumber2 +".png");
+
+//If player1 wins
+if (randomNumber1 >randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+//If player 2 wins
+if (randomNumber1 <randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+//For a draw
+if(randomNumber1===randomNumber2){
+  document.querySelector("h1").innerHTML = "Draw";
+
+}
