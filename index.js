717cef2a67dcb5de631e61fbf653a5414c4596ad
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2020-04-27 12:00:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor((Math.random()*6)+1) ;
 var randomNumber2 = Math.floor((Math.random()*6)+1) ;
 
-document.querySelector(".img1").setAttribute("src" , "images\\dice" + randomNumber1 +".png");
-document.querySelector(".img2").setAttribute("src" , "images\\dice" + randomNumber2 +".png");
+document.querySelector(".img1").setAttribute("src" , "dice" + randomNumber1 +".png");
+document.querySelector(".img2").setAttribute("src" , "dice" + randomNumber2 +".png");
 
 //If player1 wins
 if (randomNumber1 >randomNumber2){
